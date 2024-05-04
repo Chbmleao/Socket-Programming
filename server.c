29d79190097b57977133978d46b2489885424ad6
Chbmleao <caikleao@gmail.com>
2024-05-04 11:32:40 -0300

    feat: add ipv6 structure to server

--- server.c
+++ server.c
@@ -9,8 +9,8 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define IPV4 1001
-#define IPV6 1002
+#define IPV4 AF_INET
+#define IPV6 AF_INET6
 #define MAXPENDING 10
 #define M_PI 3.14159265358979323846
 #define MESSAGE_SIZE 40
@@ -86,7 +86,6 @@ void handleTCPClient(int clientSocket) {
 
   // Receive message from client
   char message[MESSAGE_SIZE];
-
   ssize_t numBytesReceived = recv(clientSocket, message, sizeof(message), 0);
   sscanf(message, "%lf, %lf", &clientCoordinates.latitude, &clientCoordinates.longitude);
   if (numBytesReceived < 0) {
@@ -105,17 +104,16 @@ void handleTCPClient(int clientSocket) {
     if (numBytesSent < 0)
         exitWithSystemMessage("send() failed");
   } else {
-    int dist = round(haversine(clientCoordinates.latitude, clientCoordinates.longitude, coordServ.latitude, coordServ.longitude) * 1000);
+    int distance = round(haversine(clientCoordinates.latitude, clientCoordinates.longitude, coordServ.latitude, coordServ.longitude) * 1000);
     
     // Send received string and receive again until end of stream
     while (1) { // 0 indicates end of stream
-      char distanceMessage[countDigits(dist)+2];
-      snprintf(distanceMessage, countDigits(dist)+2, "%dm", dist);
+      char distanceMessage[countDigits(distance)+2];
+      snprintf(distanceMessage, countDigits(distance)+2, "%dm", distance);
 
       // Echo message back to client
       ssize_t numBytesSent = -1;
-      if (dist > 0) {
-        printf("Enviando: %s\n", distanceMessage);
+      if (distance > 0) {
         numBytesSent = send(clientSocket, &distanceMessage, sizeof(distanceMessage), 0);
       } else {
         numBytesSent = send(clientSocket, "DRIVER_ARRIVED", sizeof("DRIVER_ARRIVED"), 0);
@@ -125,13 +123,42 @@ void handleTCPClient(int clientSocket) {
       if (numBytesSent < 0)
         exitWithSystemMessage("send() failed");
   
-      dist -= 400;
-      sleep(1);
+      distance -= 400;
+      sleep(2);
     }
   }
 
   close(clientSocket); // Close client socket
 }
+
+union ServerAddress {
+  struct sockaddr_in serverAddressIPV4; 
+  struct sockaddr_in6 serverAddressIPV6;
+};
+
+union ServerAddress getServerAddressStructure (int ipType, in_port_t servPort) {
+  union ServerAddress serverAddress;
+  memset(&serverAddress, 0, sizeof(serverAddress)); // Zero out structure
+
+  if (ipType == IPV4) {
+    serverAddress.serverAddressIPV4.sin_family = AF_INET; // IPv4 address family
+    serverAddress.serverAddressIPV4.sin_addr.s_addr = htonl(INADDR_ANY); // Any incoming interface
+    serverAddress.serverAddressIPV4.sin_port = htons(servPort); // Local port
+  } else if (ipType == IPV6) {
+    serverAddress.serverAddressIPV6.sin6_family = AF_INET6; // IPv6 address family
+    serverAddress.serverAddressIPV6.sin6_addr = in6addr_any; // Any incoming interface
+    serverAddress.serverAddressIPV6.sin6_port = htons(servPort); // Local port
+  } else {
+    exitWithUserMessage("Invalid IP type", "IP type must be either IPV4 or IPV6");
+  }
+
+  return serverAddress;
+}
+
+union ClientAddress {
+  struct sockaddr_in clientAddressIPV4;
+  struct sockaddr_in6 clientAddressIPV6;
+};
 
 int main (int argc, char *argv[]) {
   printServerWaiting();
@@ -141,21 +168,16 @@ int main (int argc, char *argv[]) {
     exitWithUserMessage("Parameter(s)", "<Server Port>");
   }
 
-  int ipType = (strcmp(argv[1], "ipv4") == 0) ? IPV4 : IPV6;
-  in_port_t servPort = atoi(argv[2]); // First arg: local port
+  // Create server address structure
+  int ipType = (strcmp(argv[1], "ipv6") == 0) ? IPV6 : IPV4; // First arg: ip type
+  in_port_t servPort = atoi(argv[2]); // Second arg: local port
+  union ServerAddress serverAddress = getServerAddressStructure(ipType, servPort);
 
   // Create socket for incoming connections
   int serverSock; // Socket descriptor for server
-  if ((serverSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+  if ((serverSock = socket(ipType, SOCK_STREAM, IPPROTO_TCP)) < 0) {
     exitWithSystemMessage("socket() failed");
   }
-
-  // Construct local address structure
-  struct sockaddr_in serverAddress; // Local address
-  memset(&serverAddress, 0, sizeof(serverAddress)); // Zero out structure
-  serverAddress.sin_family = AF_INET; // IPv4 address family
-  serverAddress.sin_addr.s_addr = htonl(INADDR_ANY); // Any incoming interface
-  serverAddress.sin_port = htons(servPort); // Local port
 
   // Bind to the local address
   if (bind(serverSock, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0) {
@@ -168,11 +190,11 @@ int main (int argc, char *argv[]) {
   }
 
   while (1) {
-    struct sockaddr_in clientAddress; // Client address
-    socklen_t clientAddressLen = sizeof(clientAddress); // Set length of client address structure
+    union ClientAddress clientAddress;
+    socklen_t clientAddressLength = sizeof(clientAddress); // Set length of client address structure
 
     // Wait for a client to connect
-    int clientSock = accept(serverSock, (struct sockaddr *) &clientAddress, &clientAddressLen);
+    int clientSock = accept(serverSock, (struct sockaddr *) &clientAddress, &clientAddressLength);
     if (clientSock < 0) {
       exitWithSystemMessage("accept() failed");
     }
